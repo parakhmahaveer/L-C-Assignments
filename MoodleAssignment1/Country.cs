2a3073e15165bcc5e476cce2f4c7a81e7a72e61b
Mahaveer Parakh <mahaveer.parakh@intimetec.com>
2024-12-31 16:26:35 +0530

    Adjacent Country Finder Moodle Assignment 1

---
+++ MoodleAssignment1/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleAssignment1
+{
+    public class Country
+    {
+        public string CountryCode { get; set; }
+        public List<string> AdjacentCountries { get; set; }
+
+        public Country(string countryCode)
+        {
+            CountryCode = countryCode;
+            AdjacentCountries = new List<string>();
+        }
+
+        public void AddAdjacentCountry(string country)
+        {
+            if (!AdjacentCountries.Contains(country))
+            {
+                AdjacentCountries.Add(country);
+            }
+        }
+    }
+}
---
+++ MoodleAssignment1/CountryData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleAssignment1
+{
+    public class CountryData
+    {
+        public Dictionary<string, Country> Countries { get; private set; }
+
+        public CountryData()
+        {
+            Countries = new Dictionary<string, Country>();
+
+            InitializeCountryData();
+        }
+
+        // Initialize with some data
+        private void InitializeCountryData()
+        {
+            var india = new Country("IN");
+            india.AddAdjacentCountry("Pakistan");
+            india.AddAdjacentCountry("China");
+            india.AddAdjacentCountry("Nepal");
+            india.AddAdjacentCountry("Bangladesh");
+            india.AddAdjacentCountry("Sri Lanka");
+            india.AddAdjacentCountry("Bhutan");
+
+            var us = new Country("US");
+            us.AddAdjacentCountry("Canada");
+            us.AddAdjacentCountry("Mexico");
+
+            var newZealand = new Country("NZ");
+            newZealand.AddAdjacentCountry("Australia");
+
+            var canada = new Country("CA");
+            canada.AddAdjacentCountry("US");
+
+            var mexico = new Country("MX");
+            mexico.AddAdjacentCountry("US");
+            mexico.AddAdjacentCountry("Guatemala");
+            mexico.AddAdjacentCountry("Belize");
+
+            var australia = new Country("AU");
+            australia.AddAdjacentCountry("New Zealand");
+
+            // Adding all countries to dictionary for mapping data
+            _countries.Add("IN", india);
+            _countries.Add("US", us);
+            _countries.Add("NZ", newZealand);
+            _countries.Add("CA", canada);
+            _countries.Add("MX", mexico);
+            _countries.Add("AU", australia);
+        }
+    }
+}
