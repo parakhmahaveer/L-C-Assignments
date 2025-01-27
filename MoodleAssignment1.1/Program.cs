6bcfdbe7731e85aad500beddf428842a7daadccf
Mahaveer Parakh <mahaveer.parakh@intimetec.com>
2025-01-27 21:26:00 +0530

    Moodle Assignments: Please add the meaningful names

---
+++ MoodleAssignment1.1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MoodleAssignment1._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Roll the Dice");
+                Console.WriteLine("2. Guess the Number");
+                Console.WriteLine("3. Check Armstrong Number");
+                Console.WriteLine("4. Exit");
+                string choice = Console.ReadLine();
+
+                var numberIdentifier = new NumberIdentifier();
+                switch (choice)
+                {   
+                    case "1":
+                        var diceRoller = new DiceRoller();
+                        diceRoller.RollTheDice();
+                        break;
+                    case "2":
+                        numberIdentifier.GuessTheNumber();
+                        break;
+                    case "3":
+                        numberIdentifier.CheckArmstrongNumber();
+                        break;
+                    case "4":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
