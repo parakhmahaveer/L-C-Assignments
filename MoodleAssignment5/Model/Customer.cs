60514cd3c92c321a6d884741265490ae231be580
Mahaveer Parakh <mahaveer.parakh@intimetec.com>
2025-03-24 16:51:45 +0530

    First commit for the Object and Data Structure assignment

---
+++ MoodleAssignment5/Model/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleAssignment5.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Wallet Wallet;
+
+        public Customer(string firstName, string lastName, decimal initialBalance)
+        {
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            Wallet = new Wallet(initialBalance);
+        }
+
+        public bool MakePayment(decimal amount) => Wallet.Withdraw(amount);
+        public decimal GetBalance() => Wallet.Balance;
+    }
+}
---
+++ MoodleAssignment5/Service/PaymentService.cs
@@ -0,0 +1,18 @@
+﻿using MoodleAssignment5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleAssignment5.Service
+{
+    public class PaymentService
+    {
+        public bool ProcessPayment(Customer customer, decimal amount)
+        {
+            if (customer == null) throw new ArgumentNullException(nameof(customer));
+            return customer.MakePayment(amount);
+        }
+    }
+}
