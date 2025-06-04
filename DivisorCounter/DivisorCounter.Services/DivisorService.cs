4cb5b886cfbf97cb0f9fe47c7cf5dcaa0943b15b
Mahaveer Parakh <mahaveer.parakh@intimetec.com>
2025-06-05 00:47:42 +0530

    Renamed it and added hard coded implementation

---
+++ DivisorCounter/DivisorCounter.Services/DivisorService.cs
@@ -0,0 +1,17 @@
+﻿using DivisorCounter.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisorCounter.Services
+{
+    public class DivisorService : IDivisorService
+    {
+        public int CountDivisors(int number)
+        {
+            return 10;
+        }
+    }
+}
--- DivisorCounter/DivisorCounter.Tests/DivisorCounterTest.cs
+++ DivisorCounter/DivisorCounter.Tests/DivisorCounterTest.cs
@@ -1,11 +1,12 @@
+using DivisorCounter.Services;
+
 namespace DivisorCounter.Tests
 {
     public class DivisorCounterTest
     {
         [Fact]
-        public void No_Service_Exists_Test()
+        public void CountDivisors_Return1_For1()
         {
-            // Compilation Error
             var service = new DivisorService();
             var result = service.CountDivisors(1);
             Assert.Equal(1, result);
