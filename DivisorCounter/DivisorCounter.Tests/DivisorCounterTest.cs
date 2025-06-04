506398767fbdf43d73c12bff8bae84a3892fa2f0
Mahaveer Parakh <mahaveer.parakh@intimetec.com>
2025-06-05 00:57:49 +0530

    Test 2: failed as Input is dynamic while response is hardcoded.

--- DivisorCounter/DivisorCounter.Tests/DivisorCounterTest.cs
+++ DivisorCounter/DivisorCounter.Tests/DivisorCounterTest.cs
@@ -11,5 +11,14 @@ namespace DivisorCounter.Tests
             var result = service.CountDivisors(1);
             Assert.Equal(1, result);
         }
+
+
+        [Fact]
+        public void CountDivisors_Return3_For4()
+        {
+            var service = new DivisorService();
+            var result = service.CountDivisors(4);
+            Assert.Equal(3, result);
+        }
     }
 }
